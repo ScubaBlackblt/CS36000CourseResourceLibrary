67b3104ae209e3c7485d7db4d787ef8889441457
ScubaBlackblt <124805401+ScubaBlackblt@users.noreply.github.com>
2023-04-23 19:46:00 -0400

    Update addNewCourseToDatabase.php

--- DatabaseCode/addNewCourseToDatabase.php
+++ DatabaseCode/addNewCourseToDatabase.php
@@ -3,15 +3,13 @@ include 'connect.php';
 
 $teacherID = $userID;
 
-$sql = "INSERT INTO course (teacherID) VALUES ".$teacherID.";";
+$sql = "INSERT INTO course (teacherID) VALUES (".$teacherID.");";
 $con->query($sql);
 
-$sql = "SELECT LAST_INSERT_ID(moduleID) AS ID FROM module ORDER BY LAST_INSERT_ID(moduleID) DESC LIMIT 1;";
+$sql = "SELECT LAST_INSERT_ID(courseID) AS ID FROM course ORDER BY LAST_INSERT_ID(courseID) DESC LIMIT 1;";
 $courseID = $con->query($sql)->fetch_assoc()['ID'];
-
-$sql = "INSERT INTO category (categoryName, canHaveSubcategories, parentID) VALUES Homepage, 1,".$courseID.";";
-$con->query($sql);
-
-
 $con->close();
+include 'connect2.php';
+$sql = "INSERT INTO category (categoryName, canHaveSubcategories, parentID) VALUES ('Homepage', 1,".$courseID.");";
+$con2->query($sql);
 ?>
