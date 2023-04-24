6d23c9b7b40ac63a3055614b5dbddd47ca7684cc
meganhacha <111624182+meganhacha@users.noreply.github.com>
2023-04-24 15:41:50 -0400

    Add files via upload

--- DatabaseCode/addModuleToDatabase.php
+++ DatabaseCode/addModuleToDatabase.php
@@ -3,8 +3,10 @@ include 'connect.php';
 
 $contentType = $_POST['contentType'];
 $content = $_POST['content'];
-// $pageID = $_GET['pageID'];
-$pageID = 1;
+$pageID = $_GET['pageID'];
+// $pageID = 1;
+$userID = $_GET['userID'];
+$courseID = $_GET['courseID'];
 
 $sql = "INSERT INTO module (categoryID) VALUES (".$pageID.");";
 $con->query($sql);
@@ -16,12 +18,14 @@ $con->close();
 include 'connect2.php';
 
 if ($contentType == "submission"){
-  $sql = "INSERT INTO content (moduleID, contentType) VALUES (" .$moduleID. "," .$contentType.");";
+  $sql = "INSERT INTO content (moduleID, contentType) VALUES (".$moduleID.",'$contentType');";
   $con2->query($sql); 
 }
 else{
   $sql = "INSERT INTO content (moduleID, contentType, textentered) VALUES ( ".$moduleID.", '$contentType', '$content');";
   $con2->query($sql); 
 }
+
+header("Location: http://localhost:3000/components.php?userID=$userID&courseID=$courseID&pageID=$pageID");
 $con2->close();
 ?>
