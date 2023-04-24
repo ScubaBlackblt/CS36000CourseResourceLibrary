9af780a45a8b37a0d810be56e05694fd8ee3e312
HalieA <112346066+HalieA@users.noreply.github.com>
2023-04-23 20:07:31 -0400

    homePage files

--- Homepage.php
+++ Homepage.php
@@ -3,18 +3,14 @@
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta charset="UTF-8">
-            <title>Course Reasouce Library</title>
+            <title>Course Reasource Library</title>
             <link rel="stylesheet" href="homePageCSS.css">
-            
-            
          </head>
-       
           <body>
 <!-----------------------Class Name Block----------------------------------------------------------->
 <!----------------datbase will need to be linked in link below upon completion for full testing----------------------->
      <div class="top"> 
-     <!--  <h1> <//?php echo $courseName; ?> </h1>-->
-            
+     <!--  <h1> <//?php echo $courseName; ?> </h1>-->      
       <button class="editCourseName" onclick="openForm()">+</button>
             <div class ="form-popup" id="classForm">
                <form className="classNameForm" action="/insert" method="post">
@@ -42,9 +38,6 @@
         <!--the list that displays all cataries in side bar, js add each new list item and pulls both the course id and---------------
         -------------and the catagory name from database for creation ------------------------------------------------->
         <div class = "container" id="buttons-container">
-
-
-
         </div>
         <br>
         <br>
@@ -62,7 +55,7 @@
       </form>
 
        </div> 
-       <a class = "addStudent" ahref = "" >  Add Student</a>
+       <button type= "button" id = "addStudent" onclick="changePage()" >  Add Student</a>
 
 
       </div>
@@ -72,151 +65,63 @@
 
       <div class ="main" id="contentSection"> 
         <button type = "button" id = "backToHome"><</button>           
-
-
-
       </div>
     
       
+      <div class="trigger-div"></div>
+    <button class="trigger">
+        +
+    </button>
+    </div>
+    <div class="list-div">
+        <ul class="list">
+        </ul>
+    </div>
+    <div class="show-added">
+    </div>
+    <div class="modal">
+        <div class="modal-contents">
+            <button class="add-image">Image
+            </button>
+            <button class="add-file">File
+            </button>
+            <button class="add-text">Text</button>
+            <button class="add-submission">Submission
+            </button>
+            <form action="addModuleToDatabase.php" method="post" id="text-form">
+                <input type="text" id="text-input" name="content">
+                <input type="submit" name="text-submit" />
+                <input name="contentType" value="text" style="visibility: hidden">
+            </form>
+            <form action="addModuleToDatabase.php" method="post" id="file-form">
+                <input type="file" id="selected-file" name="content"> </input>
+                <input type="submit" name="file-submit" />
+                <input name="contentType" value="file" style="visibility: hidden">
+            </form>
+            <form action="addModuleToDatabase.php" method="post" id="photo-form">
+                <input type="file" id="selected-photo" name="content"></input>
+                <input type="submit" name="submit-photo" />
+                <input name="contentType" value="image" style="visibility: hidden">
+            </form>
+            <form action="addModuleToDatabase.php" method="post" id="submission-form">
+                <input type="file" id="selected-submission" name="content"></input>
+                <input type="submit" name="submit-submission"></input>
+                <input name="contentType" value="submission" style="visibility: hidden">
+            </form>
+            <span class="close-button">x</span>
+        </div>
+    </div>
+
+
+
 
 
      
+     
       
     <!-------------------------add components-------------------------------------------->
-
+   
      </body>
      <script src="homePageJS.js"></script>
-     <script>
-      
-        
-      const triggerDiv = document.createElement('div');
-      triggerDiv.className = "trigger-div"; 
-      document.body.appendChild(triggerDiv);
-      
-      const triggerButton = document.createElement('button');
-      triggerButton.innerText = '+';
-      triggerButton.className = "trigger";
-      triggerDiv.appendChild(triggerButton);
-      
-      const listDiv = document.createElement('div');
-      listDiv.className = "list-div";
-      document.body.appendChild(listDiv);
-
-      const modal = document.createElement('div');
-      modal.className = "modal";
-      document.body.appendChild(modal);
-
-      const modalContents = document.createElement('div');
-      modalContents.className= "modal-contents";
-      modal.appendChild(modalContents);
-
-      const imageButton = document.createElement('button');
-      imageButton.innerText = 'Image';
-      imageButton.className = "add-image";
-      modalContents.appendChild(imageButton);
-
-      const fileButton = document.createElement('button');
-      fileButton.innerText = 'File';
-      fileButton.className = "add-file";
-      modalContents.appendChild(fileButton);
-
-      const textButton = document.createElement('button');
-      textButton.innerText = 'Text';
-      textButton.className = "add-text";
-      modalContents.appendChild(textButton);
-
-      const submissionButton = document.createElement('button');
-      submissionButton.innerText = 'Submission';
-      submissionButton.className = "add-submission";
-      modalContents.appendChild(submissionButton);
-
-      var userText = document.createElement("INPUT");
-      userText.setAttribute("type", "text");
-      userText.className = "text-input";
-      modalContents.appendChild(userText);
-
-      var selectedFile = document.createElement("INPUT");
-      selectedFile.setAttribute("type", "file");
-      selectedFile.className = "selected-file";
-      modalContents.appendChild(selectedFile);
-
-      const fileSubmitButton = document.createElement('button');
-      fileSubmitButton.innerText = 'Submit';
-      fileSubmitButton.className = "submit-file";
-      modalContents.appendChild(fileSubmitButton);
-
-      const textSubmitButton = document.createElement('button');
-      textSubmitButton.innerText = 'Submit';
-      textSubmitButton.className = "submit-text";
-      modalContents.appendChild(textSubmitButton);
-
-      const closeButton = document.createElement('span');
-      closeButton.innerText = 'x';
-      closeButton.className = "close-button";
-      modalContents.appendChild(closeButton);
-
-
-      triggerButton.addEventListener('click', () => {
-          modal.classList.toggle("show-modal");
-          selectedFile.style.visibility = 'hidden';
-          userText.value = userText.defaultValue;
-      })
-
-      window.addEventListener('click', () => {
-          if(event.target === modal) {
-              modal.classList.toggle("show-modal");
-          }
-      })
-
-      imageButton.addEventListener('click', () => {
-          selectedFile.style.visibility = 'visible';
-          userText.style.visibility = 'hidden';
-          textSubmitButton.style.visibility = 'hidden';
-
-          let pickedImg = selectedFile.src;
-      })
-
-      fileButton.addEventListener('click', () => {
-          selectedFile.style.visibility = 'visible';
-          fileSubmitButton.style.visibility = 'visible';
-          userText.style.visibility = 'hidden';
-          textSubmitButton.style.visibility = 'hidden';
-      })
-
-      fileSubmitButton.addEventListener('click', () => {
-          var file = selectedFile.files[0].name;
-          let fileNameInput = document.createTextNode(file);
-
-          modal.classList.toggle("show-modal");
-          addToList(fileNameInput);
-      })
-
-      textButton.addEventListener('click', () => {
-          selectedFile.style.visibility = 'hidden';
-          fileSubmitButton.style.visibility = 'hidden';
-          userText.style.visibility = 'visible';
-          textSubmitButton.style.visibility = 'visible';
-
-      })
-
-      textSubmitButton.addEventListener('click', () => {
-          userInput = userText.value;
-          let insertTextInput = document.createTextNode(userInput);
-
-          modal.classList.toggle("show-modal");
-          addToList(insertTextInput);
-      })
-
-      function addToList(child) {
-          const list = document.createElement("li");
-          list.style.listStyle = "none"
-          list.appendChild(child);
-          if(listDiv.hasChildNodes()) {
-              listDiv.insertBefore(list, listDiv.firstChild);
-          }
-          else {
-              listDiv.appendChild(list);
-          }
-      }
-    </script>
+     
 </html>
--- homePageJS.js
+++ homePageJS.js
@@ -1,65 +1,243 @@
- ////class form js
-  function openForm() {
-           document.getElementById("classForm").style.display = "block"
-          }  
-  function closeForm(){
-              document.getElementById("classForm").style.display = "none"
-             }
 
-  let cNameForm = document.getElementById("classNameForm");
-  
-  cNameForm.addEventListener("submit", (e)=> {
-     e.preventDefault()          
-      let cName = document.getElementById("cName");
-          if(cName.value == ""){
-               alert("Please ensure the field is filled")
-           }
-           else{
-               alert("Class Name Updated, page may need reloded for update")
-            }
-                 ///document.getElementById("classNameFrom").style.display="none";
-                  //document.getElementById("cName").innerHTML = cName;
-  
-            });
+           ////class form js
+  function openForm() {
+    document.getElementById("classForm").style.display = "block"
+   }  
+function closeForm(){
+       document.getElementById("classForm").style.display = "none"
+      }
+
+let cNameForm = document.getElementById("classNameForm");
+
+cNameForm.addEventListener("submit", (e)=> {
+e.preventDefault()          
+let cName = document.getElementById("cName");
+   if(cName.value == ""){
+        alert("Please ensure the field is filled")
+    }
+    else{
+        alert("Class Name Updated, page may need reloded for update")
+     }
+          ///document.getElementById("classNameFrom").style.display="none";
+           //document.getElementById("cName").innerHTML = cName;
+
+     });
 
 
 //side navigation js
-  function openNav() {
-     document.getElementById("mySidenav").style.width = "250px";
-    }
-         
-    function closeNav() {
-     document.getElementById("mySidenav").style.width = "0";
-    }
-
-    //catagory popup js
-    function openCatPopup(){
-      document.getElementById("catPopup").style.display="block";
+function openNav() {
+document.getElementById("mySidenav").style.width = "250px";
+}
   
-    }
-    function closeCatPopup(){
-      document.getElementById("catPopup").style.display="none";
-  
-    }
-  //catagory list js
-  const container = document.getElementById("buttons-container");
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+}
 
-  // Loop through the buttons data and generate HTML code for each button
-  for (const button of buttons) {
-    const name = button.name;
-    const categoryID = button.category_id;
-    const buttonElement = document.createElement("button");
-    buttonElement.textContent = name;
-    
-    // Add an event listener to the button that reloads the page with the content of the corresponding category
-    buttonElement.addEventListener("click", () => {
-      window.location.href = `getPageData.php?category_id=${categoryID}`;
-    });
-    
-    // Add the button to the container element
-    container.appendChild(buttonElement);
-  }
-    
+//catagory popup js
+function openCatPopup(){
+document.getElementById("catPopup").style.display="block";
+
+}
+function closeCatPopup(){
+document.getElementById("catPopup").style.display="none";
+
+}
+//catagory list js
+const container = document.getElementById("buttons-container");
+
+// Loop through the buttons data and generate HTML code for each button
+for (const button of buttons) {
+const name = button.name;
+const categoryID = button.category_id;
+const buttonElement = document.createElement("button");
+buttonElement.textContent = name;
+
+// Add an event listener to the button that reloads the page with the content of the corresponding category
+buttonElement.addEventListener("click", () => {
+window.location.href = `getPageData.php?category_id=${categoryID}`;
+});
+
+// Add the button to the container element
+container.appendChild(buttonElement);
+}
+
+
+
+function changePage(courseID,pageID){
+document.location.assign("http://localhost:3000/CS36000CourseResourceLibrary-main/userRequestsPage.php?pageID="+pageID+"&courseID=" + (courseID));
+}
+function backToHomepage(courseID){
+document.location.assign("http://localhost:3000/CS36000CourseResourceLibrary-main/userRequestsPage.php?pageID=homepage&courseID=" + (courseID));
+}
+fetch("getPageData.php")
+        .then((response) => {
+            if (!response.ok) {
+                console.log("fail");
+            }
+            console.log("pass");
+            return response.json(); // Parse the JSON data.
+        })
+        .then((data) => {          
+            // This is where you handle what to do with the response.
+            console.log(data);
+            var pageData = data;
+            var courseData = data[0];
+            var pageData = data[1];
+            var subcategories = data[2];
+            var contents = data[3];
+            var submissions = data[4];
+
+           // put page load code here like calling loadPage(data); and putting above declarations at start of function
+            var imageCounter = 0;
+            var fileCounter = 0;
+            var textBoxCounter = 0;
+            var submissionCounter = 0;
+            var currentlySelected = "none";
+            var contentID = "none";
+            var moduleID = "none";
+            var contentType = "none";
+            var textEntered = "none";
+            const list = document.querySelector("list");
+            const modal = document.querySelector(".modal");
+            const modalContents = document.querySelector(".modal-contents");
+            const triggerDiv = document.querySelector(".trigger-div");
+            const trigger = document.querySelector(".trigger");
+            const closeButton = document.querySelector(".close-button");
+            const listDiv = document.querySelector(".list-div");
+            const textButton = document.querySelector(".add-text");
+            const imageButton = document.querySelector(".add-image");
+            const fileButton = document.querySelector(".add-file");
+            const submissionButton = document.querySelector(".add-submission");
+            const userText = document.getElementById("text-input");
+            const selectedFile = document.getElementById("selected-file");
+            const selectedPhoto = document.getElementById("selected-photo");
+            const selectedSubmission = document.getElementById("selected-submission");
+            var textSubmitForm = document.getElementById("text-form");
+            var fileSubmitForm = document.getElementById("file-form");
+            var photoSubmitForm = document.getElementById("photo-form");
+            var submissionSubmitForm = document.getElementById("submission-form");
+            const showAdd = document.querySelector(".show-added");
+            trigger.addEventListener('click', () => {
+                modal.classList.toggle("show-modal");
+                textSubmitForm.style.visibility = 'hidden';
+                fileSubmitForm.style.visibility = 'hidden';
+                photoSubmitForm.style.visibility = 'hidden';
+                submissionSubmitForm.style.visibility = 'hidden';
+            })
+            closeButton.addEventListener('click', () => {
+                modal.classList.toggle("show-modal");
+                textSubmitForm.style.visibility = 'hidden';
+                fileSubmitForm.style.visibility = 'hidden';
+                photoSubmitForm.style.visibility = 'hidden';
+                submissionSubmitForm.style.visibility = 'hidden';
+            })
+            window.addEventListener('click', () => {
+                if (event.target === modal) {
+                    modal.classList.toggle("show-modal");
+                    textSubmitForm.style.visibility = 'hidden';
+                    fileSubmitForm.style.visibility = 'hidden';
+                    photoSubmitForm.style.visibility = 'hidden';
+                    submissionSubmitForm.style.visibility = 'hidden';
+                }
+            })
+            imageButton.addEventListener('click', () => {
+                textSubmitForm.style.visibility = 'hidden';
+                fileSubmitForm.style.visibility = 'hidden';
+                photoSubmitForm.style.visibility = 'visible';
+                submissionSubmitForm.style.visibility = 'hidden';
+                currentlySelected = "image";
+            })
+            function submitPhoto() {
+                if (verifyImage(selectedPhoto.files[0]) == 1) {
+                    modal.classList.toggle("show-modal");
+                    var img = selectedPhoto.files[0];
+                    var imgReader = new FileReader();
+                    imgReader.onload = function(e) {
+                        var showImage = document.createElement("img");
+                        showImage.src = e.target.result;
+                        contentType = "image";
+                        textEntered = "none";
+                        addToList(showImage);
+                    }
+                    imgReader.readAsDataURL(img);
+                }
+            }
+            fileButton.addEventListener('click', () => {
+                textSubmitForm.style.visibility = 'hidden';
+                fileSubmitForm.style.visibility = 'visible';
+                photoSubmitForm.style.visibility = 'hidden';
+                submissionSubmitForm.style.visibility = 'hidden';
+                currentlySelected = "file";
+            })
+            function submitFile() {
+                var file = selectedFile.files[0].name;
+                let fileNameInput = document.createTextNode(file);
+                modal.classList.toggle("show-modal");
+                fileCounter++;
+                contentID = fileCounter;
+                moduleID = "file";
+                contentType = "file";
+                textEntered = "none";
+                addToList(fileNameInput);
+            }
+            textButton.addEventListener('click', () => {
+                textSubmitForm.style.visibility = 'visible';
+                fileSubmitForm.style.visibility = 'hidden';
+                photoSubmitForm.style.visibility = 'hidden';
+                submissionSubmitForm.style.visibility = 'hidden';
+                currentlySelected = "text";
+            })
+            submissionButton.addEventListener('click', () => {
+                textSubmitForm.style.visibility = 'hidden';
+                fileSubmitForm.style.visibility = 'hidden';
+                photoSubmitForm.style.visibility = 'hidden';
+                submissionSubmitForm.style.visibility = 'visible';
+                currentlySelected = "submission";
+            })
+            function submitSubmissionFile() {
+                var chosenFile = selectedFile.files[0].name;
+                const response = confirm("Is this the file you would like to submit?");
+                if (response) {
+                    //This is where it will be sent to the database.
+                    submissionCounter++;
+                    contentID = submissionCounter;
+                    moduleID = "sub";
+                    contentType = "submission";
+                    textEntered = "none";
+                    alert("Your assignment was submitted!");
+                    modal.classList.toggle("show-modal");
+                }
+            }
+            function addToList(child) {
+                const list = document.createElement("li");
+                list.style.listStyle = "none"
+                list.appendChild(child);
+                listDiv.appendChild(list);
+            }
+            for (var i = 0; i < contents.length; i++) {
+                //Data order: contentID, moduleID, contentType, textEntered;
+                var contentID = contents[i].contentID;
+                var moduleID = contents[i].moduleID;
+                var contentType = contents[i].contentType;
+                var textEntered = contents[i].textEntered;
+                    let insertTextInput = document.createTextNode(textEntered);
+                    addToList(insertTextInput);
+            }
+            function verifyImage(file) {
+                let filename = file.name;
+                const fileExt = filename.split('.').pop();
+                if (fileExt == "jpg" || fileExt == "png") {
+                    return 1;
+                } else {
+                    alert("Please select a valid photo type (.jpg or .png)");
+                    return -1;
+                }
+            }
+        })
+        .catch((error) => {
+            // This is where you handle errors.
+        });
+
 
 
   //    function showCatagory(catagory_id){
