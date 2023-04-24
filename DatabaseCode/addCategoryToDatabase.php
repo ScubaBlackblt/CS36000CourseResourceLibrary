1f9d4c785c1ce3031ee3179b925d420d59079481
ScubaBlackblt <124805401+ScubaBlackblt@users.noreply.github.com>
2023-04-23 22:19:48 -0400

    Update addCategoryToDatabase.php

--- DatabaseCode/addCategoryToDatabase.php
+++ DatabaseCode/addCategoryToDatabase.php
@@ -1,13 +1,28 @@
 <?php
-include 'connect.php';
-
+include 'connect2.php';
+global $courseID;
+$gotHomepageID = false;
 $canHaveSubcategories = $_POST['canHaveSubcategories'];
 $categoryName = $_POST['name'];
 $parentID = $_GET['pageID'];
+$userID = $_GET['userID'];
+$courseID = $_GET['courseID'];
+if ($parentID == "homepage"){
+    include 'getCourseHomepageID.php';
+    $parentID = $homePageID;
+    $gotHomepageID = true;
+}
 
-$sql = "INSERT INTO category (categoryName, canHaveSubcategories, parentID) VALUES ".$categoryName.",".$canHaveSubcategories.",".$parentID.";";
-$con->query($sql);
+
+$sql = "INSERT INTO category (categoryName, canHaveSubcategories, parentID) VALUES ('$categoryName',".$canHaveSubcategories.",".$parentID.");";
+$con2->query($sql);
 
 
-$con->close();
+$con2->close();
+if ($gotHomepageID){
+    header("Location: http://localhost:3000/CS36000CourseResourceLibrary-main/homepage.php?userID=$userID&courseID=$courseID&pageID=homepage");
+}
+else{
+    header("Location: http://localhost:3000/CS36000CourseResourceLibrary-main/homepage.php?userID=$userID&courseID=$courseID&pageID=$parentID");
+}
 ?>
