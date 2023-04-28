6bdc6a74c8ee1976fca6c36eafed3c482722cead
AlexKitani <112361210+AlexKitani@users.noreply.github.com>
2023-04-28 16:26:21 -0400

    Added documentation

--- DatabaseCode/getJoinRequestData.php
+++ DatabaseCode/getJoinRequestData.php
@@ -1,19 +1,29 @@
 <?php
 
+//Gets the data from the join request table for implementing on webpage
+//Returns none
+//Inputs: courseID
+//Outputs: requestArray
+
+//Open new connection
 include 'connect.php';
 
+//Get values from form post
 $courseID = $_GET['courseID'];
-//$courseID = 1;
 
+//Get the information from the database
 $sql = "SELECT joinrequesttable.courseID, user.userID, user.username FROM joinrequesttable INNER JOIN user ON joinrequesttable.userID = user.userID WHERE joinrequesttable.courseID = ".$courseID.";";
 $requests = $con->query($sql);
 
+//Create a request array
 $requestArray = array();
+//Loop to enter values into array
 if ($requests->num_rows > 0) {
     while($request = $requests->fetch_assoc()) {
         array_push($requestArray, $request);
     }
 }
 $con->close();
+//Echoes the array 
 echo json_encode($requestArray);
 ?>
