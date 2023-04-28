4c6b3766179e6f5d6da1460d4e1d633e6f4a09c7
ScubaBlackblt <124805401+ScubaBlackblt@users.noreply.github.com>
2023-04-28 14:24:17 -0400

    Added documentation

--- userRequestsPage.php
+++ userRequestsPage.php
@@ -1,10 +1,13 @@
-<!-- <form action="" method="post" id="request1">
-    <label>Temp Username</label>
-    <input name="accept/decline" type="radio" value="accept">Accept</input>
-    <input name="accept/decline" type="radio" value="decline">Decline</input>
-    <input type="submit" />
-    <input name="userID" value="1" style="visibility: hidden">
-</form> -->
+<!-- The userRequestsPage displays the currrent requests to join a course to a teacher. Clicking accept or reject then submit will 
+allow the student who's username is listed next to the selected submit button will be accepted to/rejected from accessing the course. 
+Clicking the back button will bring the user back the the courses homepage.
+Inputs: n/a
+Outputs: n/a
+By: Alec Goodrich
+Date Last Modified: 4/28/2023
+-->
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,55 +21,47 @@
 </head>
 
 <body>
-    <!-----------------------Class Name Block----------------------------------------------------------->
-    <!----------------datbase will need to be linked in link below upon completion for full testing----------------------->
+    <!-- To top bar of the page -->
     <div class="top">
-        <!--  <h1> <//?php echo $courseName; ?> </h1>-->
         <button type = "button" id = "backToHome" style="position: absolute; top: 2%; left: 1%; padding: 10px;"><</button>
         <div id="pageName" style="font-size: 2.5em;">Add Students to Course</div>
         <script>
+        // Adds listner to back button that when clicked goes back to homepage
+        // Inputs: n/a
+        // Outputs: n/a
         let backButton = document.getElementById("backToHome");
         backButton.addEventListener('click', () => {
             const queryString = window.location.search;
             const urlParams = new URLSearchParams(queryString);
             const courseID = urlParams.get('courseID');
             const userID = urlParams.get('userID');
+            // Go back to homepage
             document.location.assign("http://localhost:3000/CS36000CourseResourceLibrary-main/homepage.php?pageID=homepage&courseID="+courseID+"&userID="+userID);
 
         })
 </script>
     </div>
-
-
-
-    <!-------------------------------------------------------->
-
-
-
-
-
-
-    <!-------------------------add components-------------------------------------------->
 </body>
 <div id="requests" style="margin:auto; text-align: center;">
 
 </div>
 <script>
+    // Add the user requests to the page by getting values from the URL and then accesing the database to get request data
+    // Inputs: n/a
+    // Outputs: n/a
+
+    //Get URL values
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const courseID = urlParams.get('courseID');
+
+    // Get request data from database
     fetch("DatabaseCode/getJoinRequestData.php?courseID="+courseID)
         .then((response) => {
-            if(!response.ok){ 
-                console.log("fail");
-            }
-            console.log("pass");
+            // Return data in a useable format
             return response.json(); // Parse the JSON data.
         })
         .then((data) => {
-            // This is where you handle what to do with the response.
-            console.log(data);
-            //put page load code here like calling loadPage(data); and putting above declarations at start of function
             for (var i = 0; i < data.length; i++){
                 addJoinRequestToPage(data[i].username, data[i].userID)
             }
@@ -74,6 +69,11 @@
         .catch((error) => {
              // This is where you handle errors.
     });
+
+
+    // Add the join requests to the page
+    // Inputs: username, userID
+    // Output: n/a
     function addJoinRequestToPage(username, userID) {
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString);
