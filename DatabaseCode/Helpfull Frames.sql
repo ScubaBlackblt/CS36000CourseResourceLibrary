80d5354d82b436035f31cad4b6e264c9e63bb22b
ScubaBlackblt <124805401+ScubaBlackblt@users.noreply.github.com>
2023-04-28 19:55:30 -0400

    Delete Helpfull Frames.sql

--- DatabaseCode/Helpfull Frames.sql
+++
@@ -1,6 +0,0 @@
---To find last id created
-SELECT LAST_INSERT_ID(ID_colume_name) FROM table_name ORDER BY LAST_INSERT_ID(ID_colume_name) DESC LIMIT 1;
-
---Find modules and content for current page
-SELECT module.moduleID, module.contentID FROM moduletable INNER JOIN module ON module.moduleID = moduletable.moduleID WHERE moduleTableID = 1
-ORDER BY module.moduleID
